3db680c1bc2f72fb272858e8f718a1dbe7f8e022
RonaKANE <kane.arouna94@gmail.com>
2017-01-19 15:47:21 +0100

    add, show and delete a host done : every host belongs to a user in this configuration

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,20 @@
 class HostsController < ApplicationController
+	def create
+    @user = User.find(params[:user_id])
+    @host = @user.hosts.create(host_params)
+    @host.inspect
+    redirect_to user_path(@user)
+
+  end
+  def destroy
+    @user = User.find(params[:user_id])
+    @host = @user.hosts.find(params[:id])
+    @host.destroy
+    redirect_to user_path(@user)
+  end
+ 
+  private
+    def host_params
+      params.require(:host).permit(:hostname, :serial_number, :reference_orange, :location, :project_name, :brand, :model, :electricity_consumption, :state)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	has_many :hosts, dependent: :destroy
+	validates :user_id, presence: true
 end
